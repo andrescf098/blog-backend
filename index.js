7d7f17e5f95e01975d97c70ee44c0170613c2bf0
Andrés Cifuentes <andrescf@outlook.com>
2023-12-02 18:01:25 -0500

    agregando permisos

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const connection = require("./db/connection.js");
-const cors = require("cors");
-const { config } = require("./config/index.js");
-const routerApi = require("./routes/index.js");
+const express = require('express');
+const connection = require('./db/connection.js');
+const cors = require('cors');
+const { config } = require('./config/index.js');
+const routerApi = require('./routes/index.js');
 const {
   logErrors,
   errorHandler,
   boomErrorHandler,
-} = require("./middlewares/error.handler");
+} = require('./middlewares/error.handler');
 
 const app = express();
 
-const whiteListCORS = ["http://localhost:5173"];
+const whiteListCORS = ['http://localhost:5173'];
 const options = {
   origin: (origin, callback) => {
     if (whiteListCORS.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed"));
+      callback(new Error('Not allowed'));
     }
   },
 };
-app.use(cors(options));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 routerApi(app);
 app.listen(config.port, () => {
-  console.log("Server is running on port ", config.port);
+  console.log('Server is running on port ', config.port);
 });
 
 connection();
 
-require("./utils/auth/auth.util.js");
+require('./utils/auth/auth.util.js');
 
 app.use(boomErrorHandler);
 app.use(logErrors);
